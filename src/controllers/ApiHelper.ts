85be5e9c71007111a9a4bc4b31ca07bf32246667
QuintinChris <quintinchrisdev@gmail.com>
2021-07-15 08:25:43 -0400

    refactored, renamed GetIssues -> ApiHelper

--- src/controllers/ApiHelper.ts
+++ src/controllers/ApiHelper.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 const GithubApiUrl = 'https://api.github.com/'
 const SearchIssuesGithubApiUrl = 'https://api.github.com/search/issues'
+const generalQueryFilter = '-linked:pr no:assignee state:open'
 
 enum DefaultLabels {
     'good first issue',
@@ -20,6 +21,13 @@ enum KeywordLocation {
     body = 0,
     title = 1,
     comments = 2
+}
+
+type Query = {
+    languageQuery: string,
+    labelsQuery: string,
+    keywordQuery: string,
+    excludedItems: string
 }
 
 const getIssuesFromGithub = async (url: string, query: string) => {
@@ -67,34 +75,27 @@ const getLanguageQuery = (toInclude: string[], toExclude: string[]) => {
     return { includedLanguages, excludedLanguages }
 }
 
-const getExcludedItems = (items: string[]) => {}
+const getExcludedItems = (items: string[]) => {
+    let excludedItemsQuery: string = ''
+    for (let i = 0; i < items.length; i++) {
+        excludedItemsQuery += `no:${items[i]}`
+    }
+    return excludedItemsQuery
+}
 
-module.exports = getIssuesFromGithub;
+const constructQuery = (labelsQuery: string, languageQuery: string, keywordQuery: string, excludedItems: string) => {
+    const query: string = `${labelsQuery} ${languageQuery} ${keywordQuery} ${excludedItems}`;
+    return query
+}
 
+const constructQueryAndCallAPI = () => {
+    // Do stuff here, get final query, call API with that query + generalQueryFilter
+}
 
-/*
---SORTING OPTIONS--
-1. SORT BY LAST UPDATED
+const sortQueryResults = (data: Object) => {
+    // Sort results here
+    // default = by last updated
+    // give user sorting options?
+}
 
---FILTER OUT BEFORE SHOWING USER--
-1. -linked:pr
-2. no:assignee
-3. is:open / state:open
-
-
---FILTERING OPTIONS--
-SO YOU CAN QUERY GITHUB API KINDA LIKE JQL FOR JIRA,
-✔ FILTER BY LANGUAGE => LANGUAGE:TS/JS/RUBY/ETC, 
-✔ FILTER FOR TERMS IN BODY OF ISSUE => TESTS IN:BODY 
-✔ FILTER BY PRIORITY LABELS => LABEL:PRIORITY
-SEARCH THINGS THAT HAVE NO SOMETHING => NO:ASSIGNEE, NO:LABEL
-✔ SEARCH WORDS IN THE TITLE, BODY, OR COMMENTS => WORDS IN:TITLE/BODY/COMMENTS
-FILTER OUT ISSUES LINKED TO A PR => -LINKED:PR
-FILTER OUT ISSUES THAT HAVE SOMETHING => -LABEL:RESOLVED, -STATE:CLOSED, ETC
-
-IDEA IS TO LET USER CONTROL/CHOOSE WHAT THEY WANT TO FILTER BY
-SO ON THE FRONT END, WE WILL HAVE A FORM WITH BUTTONS/TEXTBOXES TO LET USERS TELL WHAT ISSUES THEY WANT TO SEE
-I.E.
-    LABELS => BUTTONS FOR EACH LABEL, THEY CAN CHOOSE MORE THAN 1
-    KEYWORDS => TEXTBOX, THEY ENTER TEXT THEY WANT TO SEE CONTAINED WITHIN (TITLE, BODY, OR COMMENTS? BUTTONS FOR THOSE?)
-*/
+module.exports = {getLabelsQuery, getLanguageQuery, getKeywordQuery, getExcludedItems, constructQueryAndCallAPI};
